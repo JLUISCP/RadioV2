613a069566f569235442b09bdc596ac41a1f2f1c
Jose Luis Carvajal Pacheco <58019262+JLUISCP@users.noreply.github.com>
2021-12-09 17:33:58 -0600

    Se agregan comprobacion para evitar NombreUsuario duplicados

--- Radio/VISTAS/CRUDUsuario.xaml.cs
+++ Radio/VISTAS/CRUDUsuario.xaml.cs
@@ -85,8 +85,15 @@ namespace Radio.VISTAS
                 usuario.Contraseña = tbContraseña.Text;
                 usuario.IdRadio = 1;
                 usuario.IdRol = ((Rol)cbRol.SelectedItem).IdRol;
-                UsuarioDAO.registrarUsuario(usuario);
-                MessageBox.Show("Usuario registrado");
+                bool registrado = UsuarioDAO.registrarUsuario(usuario);
+                if (registrado)
+                {
+                    MessageBox.Show("Usuario registrado");
+                }
+                else
+                {
+                    MessageBox.Show("No fue posible registrar, el nombre de usuario ya se encuentra registrado");
+                }
                 cargarUsuarios();
                 limpiarCampos();
             }
@@ -119,8 +126,15 @@ namespace Radio.VISTAS
                 usuarioSeleccionado.NombreUsuario = tbNombreUsuario.Text;
                 usuarioSeleccionado.Contraseña = tbContraseña.Text;
                 usuarioSeleccionado.IdRol = ((Rol)cbRol.SelectedItem).IdRol;
-                UsuarioDAO.actualizarUsuario(usuarioSeleccionado);
-                MessageBox.Show("Usuario modificado");
+                bool modificado = UsuarioDAO.actualizarUsuario(usuarioSeleccionado);
+                if (modificado)
+                {
+                    MessageBox.Show("Usuario modificado");
+                }
+                else
+                {
+                    MessageBox.Show("No fue posible actualizar el usuario, el nombre de usuario ya se encuentra registrado");
+                }
                 cargarUsuarios();
                 limpiarCampos();
             }
